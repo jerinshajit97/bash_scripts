fb986b1c448dcf8d81b60b4604b7e55c4e3cd086
+I_am_nobody+ <37446261+jerinshajit97@users.noreply.github.com>
2020-10-02 15:33:30 +0530

    Update tp.sh

--- telegram_ping/tp.sh
+++ telegram_ping/tp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 API=key
-curl -s -o /dev/null --data chat_id="550682702" --data "text=$1, $(date)" "https://api.telegram.org/bot$API/sendMessage"
+curl -s -o /dev/null --data chat_id="id" --data "text=$1, $(date)" "https://api.telegram.org/bot$API/sendMessage"
 
